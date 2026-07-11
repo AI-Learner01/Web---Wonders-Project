479f096c4d8f287931d508c6af33342adf33deda
Dnyanesh-Gavali <dnyaneshgavali07@gmail.com>
2026-07-11 11:40:34 +0530

    Created the popular destination and explore more destinations section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import Destinations from './pages/Destinations';
 import SearchBar from './components/SearchBar';
+import DestinationCard from './components/DestinationCard';
 
 function App() {
 
   return(
     <>
-     <Destinations />
+    <div className="overflow-x-hidden">
+    {/* whole page */}
+     <Destinations/>
+</div>
     </>
   )
 }
