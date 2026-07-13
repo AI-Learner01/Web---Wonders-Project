9c85cca50f602568da0cd87bd7563d3bf7b47499
Vivek <va38267@gmail.com>
2026-07-13 15:56:14 +0530

    made  packages-booking folder inside src and move my all content in it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,8 @@
-import { useState } from "react";
+import PackagesApp from "./packages-booking/PackagesApp";
 
-import Packages from "./pages/Packages";
-import Booking from "./pages/Booking";
-import BookingSummary from "./pages/BookingSummary";
+function App(){
 
-function App() {
-
-    const [page, setPage] = useState("packages");
-
-    const [selectedPackage, setSelectedPackage] = useState(null);
-
-    const [bookingData, setBookingData] = useState(null);
-
-    const handleBookNow = (tourPackage) => {
-
-        setSelectedPackage(tourPackage);
-
-        setPage("booking");
-
-    };
-
-    return (
-
-        <>
-            {page === "packages" && (
-
-                <Packages onBookNow={handleBookNow} />
-
-            )}
-
-            {page === "booking" && (
-
-                <Booking
-                    selectedPackage={selectedPackage}
-                    onBack={() => setPage("packages")}
-                    onContinue={(data) => {
-
-                        setBookingData(data);
-
-                        setPage("summary");
-
-                    }}
-                />
-
-            )}
-
-            {page === "summary" && (
-                <BookingSummary
-                    selectedPackage={selectedPackage}
-                    bookingData={bookingData}
-                    onBack={() => setPage("booking")}
-                />
-            )}
-        </>
-
-    );
+    return <PackagesApp />;
 
 }
 
---
+++ client/src/packages-booking/PackagesApp.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+import Packages from "./pages/Packages";
+import Booking from "./pages/Booking";
+import BookingSummary from "./pages/BookingSummary";
+
+function PackagesApp() {
+
+    const [page, setPage] = useState("packages");
+
+    const [selectedPackage, setSelectedPackage] = useState(null);
+
+    const [bookingData, setBookingData] = useState(null);
+
+    const handleBookNow = (tourPackage) => {
+
+        setSelectedPackage(tourPackage);
+
+        setPage("booking");
+
+    };
+
+    return (
+
+        <>
+            {page === "packages" && (
+
+                <Packages onBookNow={handleBookNow} />
+
+            )}
+
+            {page === "booking" && (
+
+                <Booking
+                    selectedPackage={selectedPackage}
+                    onBack={() => setPage("packages")}
+                    onContinue={(data) => {
+
+                        setBookingData(data);
+
+                        setPage("summary");
+
+                    }}
+                />
+
+            )}
+
+            {page === "summary" && (
+                <BookingSummary
+                    selectedPackage={selectedPackage}
+                    bookingData={bookingData}
+                    onBack={() => setPage("booking")}
+                />
+            )}
+        </>
+
+    );
+
+}
+
+export default PackagesApp;
