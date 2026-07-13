580bb7c1e6a424e61ebf29ce6a8b1acaf0ee1130
Shivansh-SVNIT <ds992568@gmail.com>
2026-07-13 16:01:58 +0530

    Update For Generalization of code

---
+++ client/src/authentication/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// React Router se zaroori cheezein import karein
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/authetication/Login';  
+import Signup from './pages/authetication/Signup'; 
+import Contact from './pages/authetication/Contact';
+import About from './pages/authetication/About';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Jab koi sirf website open kare (path="/"), toh wo direct /login par chala jaye */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        {/* Login aur Signup ke alag-alag raste (paths) */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
