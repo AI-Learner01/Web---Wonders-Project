7c8f175e66b1bee6e0f63dea929940525bdcec3e
Mayank Rawandhe <mayankrawandhe.09@gmail.com>
2026-07-14 15:41:25 +0530

    rename the homepage

--- client/src/homepage/Homeapp.jsx
+++ client/src/homepage/Homeapp.jsx
@@ -2,23 +2,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// 1. Import the Home page you just built!
-// (Make sure the path matches where you saved it)
+
 import Home from './pages/Home'; 
 
-function App() {
+function HomeApp() {
   return (
     <Router>
       <Routes>
         {/* 2. Tell React to load your Home component when users visit the main link */}
         <Route path="/" element={<Home />} />
         
-        {/* Your team can add their routes here later, like: */}
-        {/* <Route path="/destinations" element={<Destinations />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default HomeApp;
